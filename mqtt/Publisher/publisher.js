e3ab2e65000dc9c3067b73b665d5022e7add23a4
leonie-mueller <Leonie.mueller@stud.hs-kempten.de>
2024-11-20 10:38:26 +0100

    deleted stuff we dont need anymore

--- mqtt/Publisher/publisher.js
+++
@@ -1,7 +0,0 @@
-const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://localhost');
-
-client.on('connect', () => {
-    client.publish('test/topic', 'Hello from Node.js');
-    client.end();
-});
